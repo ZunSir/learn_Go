93754fadbdbd685c66cd75a9adc268b7c911f4b6
ZunSir <lengzun@163.com>
2024-08-07 11:30:34 +0800

    函数式编程

--- syntax/main.go
+++ syntax/main.go
@@ -3,5 +3,17 @@ package main
 import "bash_go/syntax/funcs"
 
 func main() {
-	funcs.Recursive(10)
+
+	// funcs.Recursive(10) // 递归调用
+	// funcs.UserFunctional1()
+	// funcs.Functional5()
+	// getName := funcs.Functional6("小明")
+	// println(getName())
+	// funcs.CallYourName()
+	// funcs.Defer()
+	// funcs.DeferClosure()
+	// funcs.DeferClosure2()
+	// funcs.DeferClosureLoopV1()
+	// funcs.DeferClosureLoopV2()
+	funcs.DeferClosureLoopV3()
 }
